23b3c0374c7c434d79decd50bb2382cab36afacd
Felipe Martinez <pipernene@gmail.com>
2019-02-26 21:21:24 +0100

    Remove unecessary code

--- ClassImpl/Implementer.cs
+++ ClassImpl/Implementer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
-using TExecutor = System.Action<System.Reflection.MethodBase, object[]>;
 
 namespace ClassImpl
 {
@@ -84,104 +83,6 @@ namespace ClassImpl
             TypeFields.Add(field, value);
 
             return field;
-        }
-
-        private MethodInfo GetMethod(string name, Type[] parameters)
-        {
-            var matching = Methods.Where(o => o.Name == name).ToArray();
-
-            if (matching.Length == 0)
-                throw new KeyNotFoundException($"Method with name {name} not found in type {typeof(TInterface)}");
-
-            if (matching.Length > 1)
-            {
-                matching = matching
-                    .Where(o => o.GetParameters().Select(i => i.ParameterType).SequenceEqual(parameters))
-                    .ToArray();
-
-                if (matching.Length == 0)
-                    throw new KeyNotFoundException($"Method with name {name} and {parameters.Length} parameters not found in type {typeof(TInterface)}");
-            }
-
-            return matching.Single();
-        }
-
-        internal object CreateImplementation(TExecutor executor)
-        {
-            /* This is how the generated class would look:
-             * class <>ImplProxyName
-             * {
-             *      private readonly TExecutor Executor;
-             * 
-             *      public <>ImplProxyName(object executor)
-             *      {
-             *          this.Executor = executor;
-             *      }
-             *      
-             *      public virtual AnImplementedMethod(int arg1, string arg2, ...)
-             *      {
-             *          this.Executor.methodToCall(
-             *              methodof(<>ImplProxyName.AnImplementedMethod),
-             *              new object[] { arg1, arg2, ... });
-             *      }
-             * }
-             * */
-
-            var interfaceType = typeof(TInterface);
-            string name = interfaceType.Name;
-
-            var ab = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName { Name = "ProxyAssembly" + name }, AssemblyBuilderAccess.Run);
-            var mb = ab.DefineDynamicModule("ProxyModule" + name);
-            var tb = mb.DefineType("<>Impl" + name, TypeAttributes.Class, interfaceType);
-            var field = tb.DefineField("Executor", typeof(TExecutor), FieldAttributes.Private | FieldAttributes.InitOnly);
-
-            //Override all interface methods
-            foreach (var item in interfaceType.GetMethods())
-            {
-                var paramTypes = item.GetParameters().Select(o => o.ParameterType).ToArray();
-                var method = tb.DefineMethod(item.Name, MethodAttributes.Public | MethodAttributes.Virtual, null, paramTypes);
-                var il = method.GetILGenerator();
-
-                //Loads the instance field in order to call its method
-                il.Emit(OpCodes.Ldarg_0);
-                il.Emit(OpCodes.Ldfld, field);
-
-                //Get the currently calling method
-                il.EmitCall(OpCodes.Call, typeof(MethodBase).GetMethod(nameof(MethodBase.GetCurrentMethod)), null);
-
-                //Creates a new array to contain all parameters
-                il.Emit(OpCodes.Ldc_I4, paramTypes.Length);
-                il.Emit(OpCodes.Newarr, typeof(object));
-
-                for (int i = 0; i < paramTypes.Length; i++)
-                {
-                    //Loads item
-                    il.Emit(OpCodes.Dup);
-                    il.Emit(OpCodes.Ldc_I4, i); //Index of the array to set
-                    il.Emit(OpCodes.Ldarg, i + 1); //Load parameter
-
-                    //Boxes it if it's a value type
-                    if (paramTypes[i].IsValueType)
-                        il.Emit(OpCodes.Box, paramTypes[i]);
-
-                    //Adds the item into the array
-                    il.Emit(OpCodes.Stelem_Ref);
-                }
-
-                il.EmitCall(OpCodes.Callvirt, typeof(TExecutor).GetMethod("Invoke"), null);
-                il.Emit(OpCodes.Ret);
-            }
-
-            var cons = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.HasThis, new[] { typeof(TExecutor) });
-            var cil = cons.GetILGenerator();
-
-            //Creates a constructor that loads the first parameter and sets it to the instance field
-            cil.Emit(OpCodes.Ldarg_0);
-            cil.Emit(OpCodes.Ldarg_1);
-            cil.Emit(OpCodes.Stfld, field);
-            cil.Emit(OpCodes.Ret);
-
-            return Activator.CreateInstance(tb.CreateTypeInfo(), executor);
         }
     }
 }
